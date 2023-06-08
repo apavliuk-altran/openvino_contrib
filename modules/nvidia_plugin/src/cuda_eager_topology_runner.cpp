c3fc9c58e6da6f947843996cb9a095f4c8be93c1
Yaroslav Torziuk <yaroslav.torziuk@capgemini.com>
2023-06-08 16:17:36 +0200

    Rename ExecGraph to EagerTopologyRunner

--- modules/nvidia_plugin/src/cuda_eager_topology_runner.cpp
+++ modules/nvidia_plugin/src/cuda_eager_topology_runner.cpp
@@ -2,21 +2,21 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "cuda_graph.hpp"
+#include "cuda_eager_topology_runner.hpp"
 
 namespace ov {
 namespace nvidia_gpu {
 
-ExecGraph::ExecGraph(const CreationContext& context, const std::shared_ptr<const ov::Model>& model)
+EagerTopologyRunner::EagerTopologyRunner(const CreationContext& context, const std::shared_ptr<const ov::Model>& model)
     : SubGraph(context, model) {}
 
-void ExecGraph::Run(const InferenceRequestContext& context, const DeviceMemBlock& memoryBlock) const {
+void EagerTopologyRunner::Run(const InferenceRequestContext& context, const DeviceMemBlock& memoryBlock) const {
     Workbuffers workbuffers{};
     workbuffers.mutable_buffers.emplace_back(memoryBlock.view().data());
     SubGraph::Execute(context, {}, {}, workbuffers);
 }
 
-const SubGraph& ExecGraph::GetSubGraph() const {
+const SubGraph& EagerTopologyRunner::GetSubGraph() const {
     return *this;
 }
 
